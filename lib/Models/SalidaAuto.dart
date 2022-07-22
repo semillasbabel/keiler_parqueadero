bff8a6362605010bf655fca8a2e572a2ccdd09e8
KeilerGB <keilercortes@grupobabel.com>
2022-07-22 15:44:16 -0600

    Agregar o Eliminar mapa listo

--- lib/Models/SalidaAuto.dart
+++ lib/Models/SalidaAuto.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: file_names
 
 import 'package:keiler_parqueadero/Models/BD.dart';
+import 'package:keiler_parqueadero/Models/ModuloFuncionamiento.dart';
 
-void ocuparparqueo(int parqueo) {
+void ocuparparqueo(int parqueo, String placa) {
   listaParqueos[parqueo].desocuparParqueo();
+  mapaOcupados.remove(placa);
 }
