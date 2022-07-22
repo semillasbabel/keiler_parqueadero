9023d10b1c5424e7a9a91a3239bc8a172d2adce6
KeilerGB <keilercortes@grupobabel.com>
2022-07-22 13:02:00 -0600

    Lista la clase para el ingreso de autos al parqueo

--- lib/Models/BD.dart
+++ lib/Models/BD.dart
@@ -1,5 +1,9 @@
 // ignore_for_file: unused_field, file_names
 
+//Declaración de Variables Globales
+bool inicializador = false;
+var listaParqueos = <Parqueos>[];
+
 //Clase Parqueos con los metodos para ocupar, desocupar y obtener precio.
 class Parqueos {
   bool _ocupado = false;
@@ -8,6 +12,54 @@ class Parqueos {
   String _nombreCliente = "";
   late DateTime _fechaEntrada;
   late DateTime _fechaSalida;
+
+  //Metodo para crear los objetos de tipo Parqueos y llenar con ellos la lista
+  void creacionParqueos() {
+    if (inicializador == false) {
+      var p1 = Parqueos();
+      var p2 = Parqueos();
+      var p3 = Parqueos();
+      var p4 = Parqueos();
+      var p5 = Parqueos();
+      var p6 = Parqueos();
+      var p7 = Parqueos();
+      var p8 = Parqueos();
+      var p9 = Parqueos();
+      var p10 = Parqueos();
+      var p11 = Parqueos();
+      var p12 = Parqueos();
+      var p13 = Parqueos();
+      var p14 = Parqueos();
+      var p15 = Parqueos();
+      var p16 = Parqueos();
+      var p17 = Parqueos();
+      var p18 = Parqueos();
+      var p19 = Parqueos();
+      var p20 = Parqueos();
+
+      listaParqueos.add(p1);
+      listaParqueos.add(p2);
+      listaParqueos.add(p3);
+      listaParqueos.add(p4);
+      listaParqueos.add(p5);
+      listaParqueos.add(p6);
+      listaParqueos.add(p7);
+      listaParqueos.add(p8);
+      listaParqueos.add(p9);
+      listaParqueos.add(p10);
+      listaParqueos.add(p11);
+      listaParqueos.add(p12);
+      listaParqueos.add(p13);
+      listaParqueos.add(p14);
+      listaParqueos.add(p15);
+      listaParqueos.add(p16);
+      listaParqueos.add(p17);
+      listaParqueos.add(p18);
+      listaParqueos.add(p19);
+      listaParqueos.add(p20);
+      inicializador = true;
+    }
+  }
 
   //Metodo para ocupar los parqueos
   void ocuparParqueo(
--- lib/Models/IngresoAuto.dart
+++ lib/Models/IngresoAuto.dart
@@ -1,1 +1,8 @@
 // ignore_for_file: file_names
+
+import 'package:keiler_parqueadero/Models/BD.dart';
+
+void ocuparparqueo(String placa, String modelo, String nombrecliente) {
+  int parqueo = 0;
+  listaParqueos[parqueo].ocuparParqueo(placa, modelo, nombrecliente);
+}
