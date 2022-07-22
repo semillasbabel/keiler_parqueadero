31f9c57c3fe01d60913a75be626daceefe0e463f
KeilerGB <keilercortes@grupobabel.com>
2022-07-22 13:05:03 -0600

    Clase desocupar parqueo lista

--- lib/Models/SalidaAuto.dart
+++ lib/Models/SalidaAuto.dart
@@ -1,2 +1,8 @@
 // ignore_for_file: file_names
 
+import 'package:keiler_parqueadero/Models/BD.dart';
+
+void ocuparparqueo(
+    int parqueo, String placa, String modelo, String nombrecliente) {
+  listaParqueos[parqueo].desocuparParqueo();
+}
