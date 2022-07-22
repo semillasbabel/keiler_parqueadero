2644a3a86b5593120b375e0e51694c784f5440b5
KeilerGB <keilercortes@grupobabel.com>
2022-07-22 11:37:38 -0600

    UI MainView Lista

--- lib/Views/MainView.dart
+++ lib/Views/MainView.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, deprecated_member_use
 
 import 'package:flutter/material.dart';
 
@@ -13,11 +13,40 @@ class _MainViewState extends State<MainView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //........................
       appBar: AppBar(
         backgroundColor: Colors.blue.shade900,
         title: const Text("PARQUEADERO KEII06"),
         centerTitle: true,
       ),
+      //........................
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          //--------------------
+          RaisedButton(
+            child: const Text("Entrada de Auto"),
+            onPressed: () {
+              _entradaAuto(context);
+            },
+          ),
+          //--------------------
+          const SizedBox(height: 60),
+          //--------------------
+          RaisedButton(
+            child: const Text("Salida de Auto"),
+            onPressed: () {
+              _salidaAuto(context);
+            },
+          ),
+          //--------------------
+        ]),
+      ),
+      //........................
     );
   }
 }
+
+void _salidaAuto(BuildContext context) {}
+
+void _entradaAuto(BuildContext context) {}
