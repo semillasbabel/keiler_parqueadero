aa815fdf8aaef9d95810d81175ceea6dba1ded1d
KeilerGB <keiler.cortes@grupobabel.com>
2022-07-28 12:34:13 -0600

    Cambios Generales

--- lib/Views/AddCarView.dart
+++ lib/Views/AddCarView.dart
@@ -107,7 +107,7 @@ class _AddCarViewState extends State<AddCarView> {
         } else {
           //Añadimos un nuevo auto
           nuevoAutoController(placaAuto, modeloAuto, nombreCliente);
-          Navigator.pushNamed(context, "MainView");
+          Navigator.pop(context, "MainView");
         }
         //-------------------------------
       } else {
